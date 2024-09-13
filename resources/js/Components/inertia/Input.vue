0f86bdd7661e0fb85a8c696b1c81921a0534fb83
NextZac <pzsebastian@gmail.com>
2024-09-14 01:14:14 +0300

    Created input component. Added brand colors. Removed unneeded code from guestLayout. Made font be Quicksand on every page

---
+++ resources/js/Components/inertia/Input.vue
@@ -0,0 +1,16 @@
+<script setup>
+import { Search } from "lucide-vue-next";
+const props = defineProps({
+    type: String,
+    placeholder: String
+})
+</script>
+
+<template>
+    <div
+        class="w-auto flex items-center gap-3 p-4 bg-brand-950 border-0 rounded-[10px] text-body disabled:opacity-50 disabled:pointer-events-none">
+        <Search class="w-5 h-5 text-brand-white" />
+        <input :type="props.type" :placeholder="props.placeholder"
+            class="bg-transparent border-transparent w-full h-full text-brand-white focus:ring-0 focus:border-transparent p-0">
+    </div>
+</template>
--- resources/js/Layouts/GuestLayout.vue
+++ resources/js/Layouts/GuestLayout.vue
@@ -1,58 +1,49 @@
 <script setup>
-import { ChevronDownIcon } from "@heroicons/vue/24/outline";
+import { ChevronDown } from "lucide-vue-next";
 import ApplicationLogo from "@/Components/inertia/ApplicationLogo.vue";
+import Input from "@/Components/inertia/Input.vue";
 import { Link } from "@inertiajs/vue3";
 </script>
 
 <template>
     <div class="min-h-screen bg-[#1E1B4B]">
         <!-- Header -->
-        <header
-            class="flex justify-between items-center px-6 py-4 bg-[#171532] shadow"
-        >
-            <!-- Logo -->
-            <div class="flex items-center space-x-10">
-                <!-- Application Logo -->
-                <Link href="/">
-                    <ApplicationLogo
-                        class="w-40 h-40 -my-14 fill-current text-gray-500"
-                    />
-                </Link>
+        <header class="flex items-center gap-[40px] px-[25px] py-[15px] bg-[#171532] overflow-hidden">
 
-                <!-- Buttons Section -->
-                <div class="hidden sm:flex space-x-10">
-                    <a class="text-white px-4 py-2 rounded"> Kinokava </a>
-                    <a
-                        class="text-white px-4 py-2 rounded rounded inline-flex items-center"
-                    >
-                        Kinod <ChevronDownIcon class="w-4 h-4 ml-2" />
-                    </a>
-                    <a class="text-white px-4 py-2 rounded"> Filmid </a>
-                    <a
-                        class="text-white px-4 py-2 rounded inline-flex items-center"
-                    >
-                        Grupikülastus
-                        <ChevronDownIcon class="w-4 h-4 ml-2" />
-                    </a>
-                    <a class="text-white px-4 py-2 rounded"> Kinkekaardid </a>
+            <!-- Application Logo -->
+            <Link href="/" class="">
+            <ApplicationLogo class="w-40 h-40 -my-14" />
+            </Link>
+
+            <!-- Buttons Section -->
+            <a href="#" class="text-white text-subtitle hover:text-brand-400">Kinokava</a>
+            <a href="#" class="group text-white text-subtitle inline-flex items-center hover:text-brand-400">
+                Kinod
+                <ChevronDown class="w-4 h-4 ml-2 transition-transform duration-300 group-hover:rotate-180" />
+            </a>
+            <a href="#" class="text-white text-subtitle hover:text-brand-400">Filmid</a>
+            <a href="#" class="group text-white text-subtitle inline-flex items-center hover:text-brand-400">
+                Grupikülastus
+                <ChevronDown class="w-4 h-4 ml-2 transition-transform duration-300 group-hover:rotate-180" />
+            </a>
+            <a href="#" class="text-white text-subtitle hover:text-brand-400">Kinkekaardid</a>
+
+            <!-- Avatar + Search -->
+            <div class="flex w-full justify-end items-center gap-[40px]">
+                <!-- Search -->
+                <Input type="text" placeholder="Otsing" />
+                <!-- Avatar -->
+                <div class="flex-shrink-0">
+                    <img src="https://via.placeholder.com/40" alt="User Avatar"
+                        class="w-10 h-10 rounded-full border-2 border-gray-300" />
                 </div>
             </div>
 
-            <!-- Avatar -->
-            <div class="flex-shrink-0">
-                <img
-                    src="https://via.placeholder.com/40"
-                    alt="User Avatar"
-                    class="w-10 h-10 rounded-full border-2 border-gray-300"
-                />
-            </div>
         </header>
 
         <!-- Main Content -->
         <main class="flex sm:justify-center items-center pt-6 sm:pt-0">
-            <div
-                class="w-full mx-4 mt-6 px-6 py-4 bg-brand-dark-darker shadow-md sm:rounded-lg"
-            >
+            <div class="w-full mx-4 mt-6 px-6 py-4 bg-brand-dark-darker sm:rounded-lg">
                 <slot />
             </div>
         </main>
